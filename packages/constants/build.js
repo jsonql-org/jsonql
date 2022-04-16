01009e0286b20f6a9971090fb4a823d2e65bc118
Joel Chu <joelckchu@gmail.com>
2022-04-16 17:16:35 +0800

    start updating the constant

--- packages/constants/build.js
+++ packages/constants/build.js
@@ -2,17 +2,19 @@
 // @2.0.0 we split in files into sections
 const fsx = require('fs-extra')
 const { join } = require('path')
+const ext = 'mjs'
+const src = join(__dirname, 'src')
 const files = [
-  'base.js',
-  'prop.js',
-  'socket.js',
-  'validation.js'
+  'base',
+  'prop',
+  'socket',
+  'validation'
 ]
 const importFn = require('./import')
-// just concat the file together as on for index.js 
-// keep getting prop not export by file error by rollup 
+// just concat the file together as on for index.js
+// keep getting prop not export by file error by rollup
 const indexOutput = files.map(file => (
-  `/* ${file} */\n\n` + fsx.readFileSync(join(__dirname, file), { encoding: 'utf8'})
+  `/* ${file} */\n\n` + fsx.readFileSync(join(src, [file, ext].join('.')), { encoding: 'utf8'})
 )).reduce((a, b) => a + '\r' + b, '')
 
 let readme = '\n'
@@ -26,9 +28,9 @@ const allProps = files.map(file => {
   readme += Object.keys(props).map(prop => `- ${prop}\n`)
   readme += '\n'
 
-  return props 
+  return props
 }).reduce((a, b) => Object.assign(a, b), {})
-// dirty hack to fix the extra character 
+// dirty hack to fix the extra character
 readme = readme.replace(/\,\-/g, '-')
 
 const strJson = `${JSON.stringify(allProps, null, 4)}`
@@ -36,17 +38,17 @@ const strJson = `${JSON.stringify(allProps, null, 4)}`
 const cjs = `module.exports = ${strJson}`
 const browser = `var jsonqlConstants = ${strJson}`
 
-// output to index 
+// output to index
 fsx.outputFileSync(join(__dirname, 'index.js'), indexOutput)
 // output to README
 const tpl = fsx.readFileSync(join(__dirname, 'README_TEMPLATE.md'))
 const content = tpl.toString().replace('[REPLACE]', readme)
 fsx.outputFileSync(join(__dirname, 'README.md'), content)
-// out put the cjs main.js  
+// out put the cjs main.js
 fsx.outputFileSync(join(__dirname, 'main.js'), cjs)
-// output to browser.js 
+// output to browser.js
 fsx.outputFileSync(join(__dirname, 'browser.js'), browser)
-// output to constants.json 
+// output to constants.json
 fsx.outputJson(join(__dirname, 'constants.json'), allProps, { spaces: 2 }, err => {
   if (err) {
     console.log('ERROR:', err)
