2b70c69ea3b0714c57bc8fa5e6390c11c885114a
Joel Chu <joelckchu@gmail.com>
2022-04-17 19:11:40 +0800

    Add the stitch together index.d.ts

---
+++ packages/constants/tsc.js
@@ -0,0 +1,25 @@
+// after running the tsc, we stitch togehter all the files
+// and dump them into the index.d.ts
+const fsx = require('fs-extra')
+const { join } = require('path')
+
+const targets = ['base', 'prop', 'socket', 'validation']
+Promise.all(
+  targets.map(target => {
+    return fsx.readFile(join(__dirname, 'tmp', `${target}.d.mts`))
+  })
+).then(results => {
+  let content = results.reduce((a, b) => {
+    return a.toString() + '\n' + b.toString()
+  }, Buffer.from(`declare module "@jsonql/constants" {`))
+  content += `\n}\n`
+
+  return content
+}).then(content => {
+
+  return fsx.outputFile(join(__dirname, 'index.d.ts'), content)
+}).then(() => {
+  console.log(`index.d.ts generated`)
+}).catch(e => {
+  console.error(`Something weng wrong`, e)
+})
