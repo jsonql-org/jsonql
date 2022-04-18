9caef2854eb0017f2b5c02916b4ada15aa9f6ebb
Joel Chu <joelckchu@gmail.com>
2022-04-18 22:59:26 +0800

    create named exports

--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -1,1 +1,95 @@
 // start your project here
+export { chainFns } from './chain-fns'
+export {
+  chainPromises,
+  chainProcessPromises,
+} from './chain-promises'
+export {
+  checkIsContract,
+  isContract,
+  extractSocketPart,
+  extractArgsFromPayload,
+  extractParamsFromContract
+} from './contract'
+export {
+  dasherize
+} from './dasherize'
+export {
+  inArray,
+  toArray,
+  parseJson,
+  isObjectHasKey,
+  createEvtName,
+  getConfigValue,
+  isNotEmpty,
+  isFunc,
+  assign,
+  nil,
+  readOnly
+} from './generic'
+export {
+  buff,
+  base64Encode,
+  base64Decode
+} from './jwt'
+export {
+  logger,
+  getLogger
+} from './logger'
+export {
+  groupPublicNamespace,
+  groupByNamespace,
+  getNamespaceInOrder,
+  getNamespace,
+  getPrivateNamespace,
+  getNspInfoByConfig,
+} from './namespace'
+export {
+  objDefineProps,
+  objHasProp,
+  injectToFn,
+} from './obj-define-props'
+export {
+  handleTimestamp,
+  toPayload,
+  formatPayload,
+  getResolverFromPayload,
+  createDeliverable,
+  createQuery,
+  createQueryStr,
+  createMutation,
+  createMutationStr,
+  getQueryFromArgs,
+  processPayload,
+  getQueryFromPayload,
+  getMutationFromArgs,
+  getMutationFromPayload
+} from './params-api'
+export {
+  isRegExp,
+  getRegex
+} from './regex'
+export {
+  getCallMethod,
+  packResult,
+  isJsonqlErrorObj,
+  packError,
+  resultHandler,
+} from './results'
+export {
+  createSendPayload,
+  getTsFieldFromData,
+  createWsReply,
+  createReplyMsg,
+  createAcknowledgeMsg,
+  isWsReply,
+  extractWsPayload,
+} from './socket'
+export {
+  timestamp
+} from './timestamp'
+export {
+  urlParams,
+  cacheBurstUrl,
+  cacheBurst
+} from './urls'
