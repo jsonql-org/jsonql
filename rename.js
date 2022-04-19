182df6e7ecb85dd13248905953f77372b8a7c3aa
Joel Chu <joelckchu@gmail.com>
2022-04-19 15:24:49 +0800

    paritally working the sub directory not getting update

--- rename.js
+++ rename.js
@@ -1,33 +1,41 @@
 #!/usr/bin/env node
 // little helpers to rename all the old js files to ts
 const args = process.argv.slice(2)
-const { join, basename } = require('path')
+const { join, basename, dirname } = require('path')
 const { exec } = require('child_process')
 const fs = require('fs-extra')
 const glob = require('glob')
-
 const dir = args[0]
-
+if (!dir) {
+  console.error(`You need to tell me where!`)
+  return
+}
 console.info(`Update all files in ${dir}`)
-
+// start
 glob(join(__dirname, 'packages', dir, '!(node_modules|dist)**', '*.js'), (err, files) => {
   if (err) {
     console.error('oops', err)
     return
   }
+  if (!files.length) {
+    console.info(`Nothing to update!`)
+    return
+  }
+
   Promise.all(
     files.map(file => {
-      console.log(file)
       const f = basename(file)
+      const d = dirname(file)
       const ts = f.replace('.js', '.ts')
+      const dest = join(d, ts)
 
-      return Promise.resolve(ts)
+      return fs.copy(file, dest)
+               .then(() => fs.remove(file))
+               .then(() => dest)
     })
   ).then(results => {
-
     console.log(`${results.length} files updated in packages/${dir}`)
-    console.log(results)
-
+    // console.log(results)
   })
 
 })
