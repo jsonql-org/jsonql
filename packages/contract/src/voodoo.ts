46b9c67428c3d0ad37b7eeb8f47450cf6d82994b
Joel Chu <joelckchu@gmail.com>
2022-04-21 08:55:42 +0800

    add more file for tet

---
+++ packages/contract/src/voodoo.ts
@@ -0,0 +1,9 @@
+
+/** Voodoo magic */
+export function magic(search?: string) {
+  let stacks = new Error().stack?.split('\n')
+  stacks = search ? stacks.filter(line => line.indexOf(search) > -1) : stacks
+  const where = stacks ? stacks[stacks.length - 1].split('(')[1].split(':')[0] : ''
+  
+return where
+}
