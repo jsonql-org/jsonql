7772db7e60bac2388537217ca5cf2bc824cf0d3d
Joel Chu <joelckchu@gmail.com>
2022-04-23 15:32:32 +0800

    setup an example plugin file

---
+++ packages/validator/src/plugins/email.ts
@@ -0,0 +1,15 @@
+// email validator
+// this is an example how to create a plugin
+// one default export method accept one parameter value return boolean
+// then export a named export call name: string and that's it
+// or just return a string regex pattern: string
+
+export const name = 'email'
+
+export const pattern = ``
+
+/*
+export default function main(value: string): boolean {
+
+}
+*/
