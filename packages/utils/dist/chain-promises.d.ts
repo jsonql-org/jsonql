88fde2d432b8f478b0720b6f40b7d9f0f8b7b59c
Joel Chu <joelckchu@gmail.com>
2022-04-25 09:09:36 +0800

    build 0.3.2

--- packages/utils/dist/chain-promises.d.ts
+++ packages/utils/dist/chain-promises.d.ts
@@ -3,12 +3,10 @@
  * and attach the auth client to it
  */
 export declare function chainPromises(promises: Array<Promise<any>>, asObject?: boolean | object): Promise<any>;
+declare type JsonqlPromiseChainFn = (...args: any[]) => Promise<any>;
 /**
  * This one return a different result from the chainPromises
  * it will be the same like chainFns that take one promise resolve as the next fn parameter
  */
-export declare function chainProcessPromises(initPromise: Function, ...promises: Array<Function>): (...args: any[]) => any;
-/**
- pass an array of promises, when one fail then exit
- then result return from the success promise will pass to the next one as parameter
-*/
+export declare function chainProcessPromises(initPromise: JsonqlPromiseChainFn, ...promises: Array<JsonqlPromiseChainFn>): (...args: any[]) => any;
+export {};
--- packages/utils/dist/chain-promises.js
+++ packages/utils/dist/chain-promises.js
@@ -10,7 +10,9 @@ const lodash_1 = require("./lodash");
  * and attach the auth client to it
  */
 function chainPromises(promises, asObject = false) {
-    return promises.reduce((promiseChain, currentTask) => (promiseChain.then(chainResults => (currentTask.then(currentResult => (asObject === false ? [...chainResults, currentResult] : (0, lodash_1.merge)(chainResults, currentResult)))))), Promise.resolve(asObject === false ? [] : ((0, lodash_1.isPlainObject)(asObject) ? asObject : {})));
+    return promises.reduce((promiseChain, currentTask) => (promiseChain.then(chainResults => (currentTask.then(currentResult => (asObject === false ?
+        [...chainResults, currentResult] :
+        (0, lodash_1.merge)(chainResults, currentResult)))))), Promise.resolve(asObject === false ? [] : ((0, lodash_1.isPlainObject)(asObject) ? asObject : {})));
 }
 exports.chainPromises = chainPromises;
 /**
@@ -21,17 +23,3 @@ function chainProcessPromises(initPromise, ...promises) {
     return (...args) => (promises.reduce((promiseChain, currentTask) => (promiseChain.then((chainResult) => (currentTask(chainResult)))), Reflect.apply(initPromise, null, args)));
 }
 exports.chainProcessPromises = chainProcessPromises;
-/**
- pass an array of promises, when one fail then exit
- then result return from the success promise will pass to the next one as parameter
-*/
-/*
-export function some(promises: Array<Promise<any>>, acculminator = []) {
-  const fn = promises.shift()
-  return fn()
-    .then((result: any) => {
-
-    })
-
-}
-*/
