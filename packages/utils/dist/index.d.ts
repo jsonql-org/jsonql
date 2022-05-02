30dd70a3c5c4ae3714245c535aea23dc6e3e6d13
Joel Chu <joelckchu@gmail.com>
2022-05-02 08:08:24 +0800

    clean up the test in utils

--- packages/utils/dist/index.d.ts
+++ packages/utils/dist/index.d.ts
@@ -1,16 +1,10 @@
 export { chainFns } from './chain-fns';
 export { chainPromises, chainProcessPromises, queuePromisesProcess, } from './chain-promises';
-export { checkIsContract, isContract, extractSocketPart, extractArgsFromPayload, extractParamsFromContract } from './contract';
 export { dasherize } from './dasherize';
 export { inArray, toArray, isEmptyObj, parseJson, objectHasKey, createEvtName, getConfigValue, isNotEmpty, notEmpty, isEmpty, isFunction, assign, nil, readOnly, showDeep, formatStr, } from './common';
-export { buff, base64Encode, base64Decode } from './jwt';
 export { logger, getLogger } from './logger';
-export { groupByNamespace, getNamespaceInOrder, getNamespace, getPrivateNamespace, getNspInfoByConfig, } from './namespace';
 export { objDefineProps, objHasProp, injectToFn, } from './obj-define-props';
-export { handleTimestamp, toPayload, formatPayload, getResolverFromPayload, createDeliverable, createQuery, createQueryStr, createMutation, createMutationStr, getQueryFromArgs, getQueryFromPayload, getMutationFromArgs, getMutationFromPayload } from './params-api';
 export { isRegExp, getRegex } from './regex';
-export { getCallMethod, packResult, isJsonqlErrorObj, packError, resultHandler, } from './results';
-export { createSendPayload, createWsReply, createReplyMsg, createAcknowledgeMsg, isWsReply, extractWsPayload, } from './socket';
 export { timestamp } from './timestamp';
 export { urlParams, cacheBurstUrl, cacheBurst } from './urls';
 export { isNaN, isString, isPlainObject, merge, mapValues, mapKeys, omitBy, findKey, curry, } from './lodash';
--- packages/utils/src/index.ts
+++ packages/utils/src/index.ts
@@ -6,13 +6,6 @@ export {
   queuePromisesProcess,
 } from './chain-promises'
 export {
-  checkIsContract,
-  isContract,
-  extractSocketPart,
-  extractArgsFromPayload,
-  extractParamsFromContract
-} from './contract'
-export {
   dasherize
 } from './dasherize'
 export {
@@ -34,63 +27,18 @@ export {
   formatStr,
 } from './common'
 export {
-  buff,
-  base64Encode,
-  base64Decode
-} from './jwt'
-export {
   logger,
   getLogger
 } from './logger'
-export {
-  // groupPublicNamespace,
-  groupByNamespace,
-  getNamespaceInOrder,
-  getNamespace,
-  getPrivateNamespace,
-  getNspInfoByConfig,
-} from './namespace'
 export {
   objDefineProps,
   objHasProp,
   injectToFn,
 } from './obj-define-props'
 export {
-  handleTimestamp,
-  toPayload,
-  formatPayload,
-  getResolverFromPayload,
-  createDeliverable,
-  createQuery,
-  createQueryStr,
-  createMutation,
-  createMutationStr,
-  getQueryFromArgs,
-  // processPayload,
-  getQueryFromPayload,
-  getMutationFromArgs,
-  getMutationFromPayload
-} from './params-api'
-export {
   isRegExp,
   getRegex
 } from './regex'
-export {
-  getCallMethod,
-  packResult,
-  isJsonqlErrorObj,
-  packError,
-  resultHandler,
-} from './results'
-export {
-  createSendPayload,
-  // getTsFieldFromData,
-  createWsReply,
-  createReplyMsg,
-  createAcknowledgeMsg,
-  isWsReply,
-  extractWsPayload,
-} from './socket'
 export {
   timestamp
 } from './timestamp'
