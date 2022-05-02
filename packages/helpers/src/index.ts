56a896f41e6751ac2b3b9966510c76bc1f24cc70
Joel Chu <joelckchu@gmail.com>
2022-05-02 08:04:52 +0800

    start moving files out from utils to helpers

--- packages/helpers/src/index.ts
+++ packages/helpers/src/index.ts
@@ -1,1 +1,114 @@
 // start your project here
+export { chainFns } from './chain-fns'
+export {
+  chainPromises,
+  chainProcessPromises,
+  queuePromisesProcess,
+} from './chain-promises'
+export {
+  checkIsContract,
+  isContract,
+  extractSocketPart,
+  extractArgsFromPayload,
+  extractParamsFromContract
+} from './contract'
+export {
+  dasherize
+} from './dasherize'
+export {
+  inArray,
+  toArray,
+  isEmptyObj,
+  parseJson,
+  objectHasKey,
+  createEvtName,
+  getConfigValue,
+  isNotEmpty,
+  notEmpty,
+  isEmpty,
+  isFunction,
+  assign,
+  nil,
+  readOnly,
+  showDeep,
+  formatStr,
+} from './common'
+export {
+  buff,
+  base64Encode,
+  base64Decode
+} from './jwt'
+export {
+  logger,
+  getLogger
+} from './logger'
+export {
+  // groupPublicNamespace,
+  groupByNamespace,
+  getNamespaceInOrder,
+  getNamespace,
+  getPrivateNamespace,
+  getNspInfoByConfig,
+} from './namespace'
+export {
+  objDefineProps,
+  objHasProp,
+  injectToFn,
+} from './obj-define-props'
+export {
+  handleTimestamp,
+  toPayload,
+  formatPayload,
+  getResolverFromPayload,
+  createDeliverable,
+  createQuery,
+  createQueryStr,
+  createMutation,
+  createMutationStr,
+  getQueryFromArgs,
+  // processPayload,
+  getQueryFromPayload,
+  getMutationFromArgs,
+  getMutationFromPayload
+} from './params-api'
+export {
+  isRegExp,
+  getRegex
+} from './regex'
+export {
+  getCallMethod,
+  packResult,
+  isJsonqlErrorObj,
+  packError,
+  resultHandler,
+} from './results'
+export {
+  createSendPayload,
+  // getTsFieldFromData,
+  createWsReply,
+  createReplyMsg,
+  createAcknowledgeMsg,
+  isWsReply,
+  extractWsPayload,
+} from './socket'
+export {
+  timestamp
+} from './timestamp'
+export {
+  urlParams,
+  cacheBurstUrl,
+  cacheBurst
+} from './urls'
+// also re-expor this for other project to use
+export {
+  isNaN,
+  isString,
+  isPlainObject,
+  merge,
+  mapValues,
+  mapKeys,
+  omitBy,
+  findKey,
+  curry,
+  // isEqual
+} from './lodash'
