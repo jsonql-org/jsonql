d1e5f15c26b532f9a7e74194013a5c4d3912003b
Joel Chu <joelckchu@gmail.com>
2022-05-09 11:07:04 +0800

    still pondering what to do with the public and private contract

--- packages/contract/src/contract.ts
+++ packages/contract/src/contract.ts
@@ -1,34 +1,50 @@
 // We now use an object style to generate contract
 // this is for the Velocejs FastApi
+import { join } from 'node:path'
+import { writeJson } from 'fs-extra'
 import {
   stripAllTypeParams
 } from '@jsonql/ast'
 import {
   readOnly,
-  isEmptyObj,
+  // isEmptyObj,
+  assign,
 } from '@jsonql/utils'
 import {
   JsonqlError
 } from '@jsonql/errors'
 import {
   REST_NAME,
+  DATA_KEY,
+  META_KEY,
+  ERROR_KEY,
+  DEFAULT_CONTRACT_DIR,
+  DEFAULT_CONTRACT_FILE_NAME,
+  PUBLIC_CONTRACT_FILE_NAME,
   // JSONQL_NAME,
 } from '@jsonql/constants'
 
-
+// main
 export class JsonqlContract {
   private _astMap: any
-  private _contract = { data: {}, meta: {}, error: {} }
+  // form the basic structure
+  private _contract = {
+    [DATA_KEY]: {},
+    [META_KEY]: {},
+    [ERROR_KEY]: {}
+  }
 
   /** instead of run the parser again we just load the ast map */
   constructor(astMap: any, type = REST_NAME) {
     this._astMap = readOnly(astMap) // keep an original
     //we are going to add props to it
-    this._contract.data = stripAllTypeParams(astMap)
-    this._contract.meta = { type }
+    this._contract[DATA_KEY] = stripAllTypeParams(astMap)
+    this._contract[META_KEY] = { type }
   }
 
   /** insert into the object and make sure it's immutatable */
+
+  /*
   protected insert(obj: any, entry: any, path: string) {
     const paths = path.split('.')
     const ctn = paths.length
@@ -38,23 +54,24 @@ export class JsonqlContract {
       if ()
     }
   }
+  */
 
+  
 
-  // we are going to have three root properties
-  public data(entry: any, path?: string) {
-    this._contract.data = entry
-  }
-
+  /** this will always overwrite the last one */
   public error(error: JsonqlError) {
-    this._contract.error = error
+    this._contract[ERROR_KEY] = error
   }
 
-  public meta(entry: any, path?: string) {
-    this._contract.meta
+  /** always make sure it's immutable */
+  public meta(entry: any) {
+    this._contract[META_KEY] = assign({}, this._contract[META_KEY], entry)
   }
 
   /** we output several different contracts all at once */
-  public write(outDir: string) {
+  public write(outDir = DEFAULT_CONTRACT_DIR) {
+    const contract = assign({}, this._contract)
+    // need to process the pubic contract
 
   }
 
--- packages/contract/tests/base.test.ts
+++ packages/contract/tests/base.test.ts
@@ -1,3 +1,21 @@
 import test from 'ava'
-
+import { REST_NAME, DATA_KEY, META_KEY, ERROR_KEY } from '@jsonql/constants'
 test.todo('You need to write some test')
+
+// develop the insert method
+const contractObj = {
+  [DATA_KEY]: {
+    className: 'SomeClass'
+  },
+  [META_KEY]: {
+    type: REST_NAME
+  },
+  [ERROR_KEY]: {}
+}
+
+test(`Developing the insert method`, t => {
+
+  // const { data, body } = contractObj 
+
+
+})
