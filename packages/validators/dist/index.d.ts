3be66e4df27d6274030464e3e3e0618bd3fe88f5
Joel Chu <joelckchu@gmail.com>
2022-05-31 15:48:27 +0800

    @jsonql/validators to 0.3.1

--- packages/validators/dist/index.d.ts
+++ packages/validators/dist/index.d.ts
@@ -1,1 +1,2 @@
 export { Validators } from './validators';
+export { VALIDATE_KEY, VALIDATE_ASYNC_KEY, PLUGIN_KEY, PLUGIN_FN_KEY, PATTERN_KEY, RULES_KEY, NAME_KEY, PARAMS_KEY, } from '@jsonql/validator-core/dist/constants';
--- packages/validators/dist/index.js
+++ packages/validators/dist/index.js
@@ -1,6 +1,16 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.Validators = void 0;
+exports.PARAMS_KEY = exports.NAME_KEY = exports.RULES_KEY = exports.PATTERN_KEY = exports.PLUGIN_FN_KEY = exports.PLUGIN_KEY = exports.VALIDATE_ASYNC_KEY = exports.VALIDATE_KEY = exports.Validators = void 0;
 // start your project here
 var validators_1 = require("./validators");
 Object.defineProperty(exports, "Validators", { enumerable: true, get: function () { return validators_1.Validators; } });
+// we need to export some of the constants here as well
+var constants_1 = require("@jsonql/validator-core/dist/constants");
+Object.defineProperty(exports, "VALIDATE_KEY", { enumerable: true, get: function () { return constants_1.VALIDATE_KEY; } });
+Object.defineProperty(exports, "VALIDATE_ASYNC_KEY", { enumerable: true, get: function () { return constants_1.VALIDATE_ASYNC_KEY; } });
+Object.defineProperty(exports, "PLUGIN_KEY", { enumerable: true, get: function () { return constants_1.PLUGIN_KEY; } });
+Object.defineProperty(exports, "PLUGIN_FN_KEY", { enumerable: true, get: function () { return constants_1.PLUGIN_FN_KEY; } });
+Object.defineProperty(exports, "PATTERN_KEY", { enumerable: true, get: function () { return constants_1.PATTERN_KEY; } });
+Object.defineProperty(exports, "RULES_KEY", { enumerable: true, get: function () { return constants_1.RULES_KEY; } });
+Object.defineProperty(exports, "NAME_KEY", { enumerable: true, get: function () { return constants_1.NAME_KEY; } });
+Object.defineProperty(exports, "PARAMS_KEY", { enumerable: true, get: function () { return constants_1.PARAMS_KEY; } });
