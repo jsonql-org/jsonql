757e0d533cc111eb432e75f03a24f1d1e1518cbf
Joel Chu <joelckchu@gmail.com>
2022-06-24 10:14:27 +0800

    setup qunit in validator-core to test the browser problems

---
+++ packages/validator-core/tests/qunit/run-qunit.mjs
@@ -0,0 +1,25 @@
+// start up for qunit
+import { getConfigForQunit } from 'server-io-core/helper.mjs'
+import serverIoCore from 'server-io-core/index.mjs'
+// combine method
+async function runQunitSetup (userConfig) {
+  return getConfigForQunit(userConfig)
+    .then(serverIoCore)
+    .catch(err => {
+      console.error(err)
+    })
+}
+
+const config = {
+  port: 0,
+  webroot: [
+    '/home/joel/Projects/jsonql-org/jsonql/packages/validator-core/tests/qunit/webroot',
+    '/home/joel/Projects/jsonql-org/jsonql/packages/validator-core/tests/qunit/files'
+  ],
+  open: true,
+  reload: true,
+  testFilePattern: '*.qunit.js',
+  baseDir: '/home/joel/Projects/jsonql-org/jsonql/packages/validator-core/tests'
+}
+
+runQunitSetup(config)
