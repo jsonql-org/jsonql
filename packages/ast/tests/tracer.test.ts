14c19ad1e1b46159e1bb9b031a2d31134c729f4e
Joel Chu <joelckchu@gmail.com>
2022-08-03 22:16:13 +0800

    remove the tracer they have been move over the new repo reveng

--- packages/ast/tests/tracer.test.ts
+++ packages/ast/tests/tracer.test.ts
@@ -4,11 +4,11 @@ import { join } from 'node:path'
 
 import { tracer } from '../src/tracer'
 
-const mainFile = join(__dirname, 'fixtures', 'index.ts')
+const mainFile = join(__dirname, 'fixtures', 'trace', 'index.ts')
 
 
 test(`Should able to return an ast file`, t => {
-  const ast = tracer(mainFile, { target: 'ES6'})
+  const ast = tracer(mainFile, { target: 'es2021'})
 
   console.dir(ast, { depth: null })
 
