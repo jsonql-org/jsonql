0028425a6479df6aa92ebd0aff50bcecfa28dcf6
Joel Chu <joelckchu@gmail.com>
2022-08-12 20:39:40 +0800

    @jsonql/utils to 1.3.2

--- packages/utils/dist/promise.d.ts
+++ packages/utils/dist/promise.d.ts
@@ -1,4 +1,4 @@
 import type { AnyType } from './types';
 export declare const promise: (cb: AnyType) => Promise<unknown>;
 /** we unwrap the result to make it more generic */
-export declare function processAll(promises: Array<Promise<AnyType>>): Promise<any[]>;
+export declare function processAll(promises: Array<Promise<AnyType>>): Promise<Array<AnyType>>;
